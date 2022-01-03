ecdb4ffa43fc796ab4cf7fb2a63eb3b80838f507
weibeu <deepakrajko14@gmail.com>
2022-01-03 22:15:55 +0530

    Add movie title type enum

---
+++ reverse_search/backends/movies/enums.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class TitleType(enum.Enum):
+
+    ALTERNATIVE = "alternative"
+    DVD = "dvd"
+    FESTIVAL = "festival"
+    TV = "tv"
+    VIDEO = "video"
+    WORKING = "working"
+    ORIGINAL = "original"
+    IMDB_DISPLAY = "imdbDisplay"
