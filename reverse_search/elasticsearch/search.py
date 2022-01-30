cf7271bdd57c8f01c341226d9992e88082323b5f
weibeu <deepakrajko14@gmail.com>
2022-01-31 04:12:20 +0530

    Fix csv field limits

--- reverse_search/elasticsearch/search.py
+++ reverse_search/elasticsearch/search.py
@@ -1,12 +1,24 @@
 from elasticsearch.helpers import bulk
 
 import csv
+import sys
 import elasticsearch
 
 
 class Elasticsearch(elasticsearch.Elasticsearch):
 
     def bulk_index_from_csv(self, filepath, index):
+        max_int = sys.maxsize
+        while True:
+            # decrease the maxInt value by factor 10
+            # as long as the OverflowError occurs.
+
+            try:
+                csv.field_size_limit(max_int)
+                break
+            except OverflowError:
+                max_int = int(max_int / 10)
+
         with open(filepath, newline=str(), encoding="utf-8") as file:
             reader = csv.DictReader(file)
             bulk(self, reader, index=index)
