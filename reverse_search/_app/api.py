640930feffdffda6e239cfd11a07bc610aa06a6c
weibeu <deepakrajko14@gmail.com>
2022-01-31 09:17:52 +0530

    Make endpoint fetch and return summarized data

--- reverse_search/_app/api.py
+++ reverse_search/_app/api.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, abort, jsonify
 import functools
 
 from ..elasticsearch import client
+from ..backends.movies.db import get_movie_details, get_subtitle_meta
 
 
 def ensure_search_query(view):
@@ -23,14 +24,32 @@ bp = Blueprint("api", __name__, url_prefix="/api", )
 @ensure_search_query
 def search_subtitles(query):
     hits = client.get_hits("title_subtitles", dialogue=query)
-    return jsonify(hits)
+    hits.sort(key=lambda h: h['_score'], reverse=True)
+
+    data = dict()
+
+    try:
+        hit = hits[0]
+    except IndexError:
+        return jsonify(data)
+    else:
+        source = hit['_source']
+        title_id = source['titleId']
+        index_ = source['index_']
+
+    data["movie_details"] = get_movie_details(title_id)
+    data["subtitle_meta"] = get_subtitle_meta(title_id, index_)
+
+    return jsonify(data)
 
 
 @bp.route("/search-movies")
 @ensure_search_query
 def search_movies(query):
     hits = client.get_hits("title_akas", title=query)
-    return jsonify(hits)
+    hits.sort(key=lambda h: h['_score'], reverse=True)
+    data = [get_movie_details(h['_source']['titleid']) for h in hits]
+    return jsonify(data)
 
 
 @bp.route("/search-names")
