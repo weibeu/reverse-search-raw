9e94a4d7287a95cc6c8024543b01b291b025385a
weibeu <deepakrajko14@gmail.com>
2022-01-31 09:08:42 +0530

    Add a function to get subtitle meta

--- reverse_search/backends/movies/db.py
+++ reverse_search/backends/movies/db.py
@@ -49,3 +49,21 @@ def get_movie_details(title_id):
 
     cur.close()
     return movie_details
+
+
+def get_subtitle_meta(title_id, index_, span=3):
+    lower_limit = index_ - span
+    upper_limit = index_ + span
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute(
+        """
+        SELECT start_, end_, dialogue
+        FROM title_subtitles
+        WHERE titleId=%s AND index_>=%s AND index_<=%s;
+        """,
+        (title_id, lower_limit, upper_limit, )
+    )
+    results = cur.fetchall()
+
+    cur.close()
+    return results
