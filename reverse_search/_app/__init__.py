9d39ff31a8023098a2b782d496b95ef5bf26c7bc
weibeu <deepakrajko14@gmail.com>
2022-01-31 09:40:35 +0530

    Inject CORS

--- reverse_search/_app/__init__.py
+++ reverse_search/_app/__init__.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask import Flask
 
 from . import api
@@ -8,6 +9,7 @@ blueprints = [api.bp, ]
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
 
     if config is not None:
         app.config.from_object(config)
