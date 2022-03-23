c35402a631db79866002dfbac6de8f0d3bdb00f5
weibeu <deepakrajko14@gmail.com>
2022-03-23 17:46:52 +0530

    Set retries and timeout limits

--- reverse_search/elasticsearch/__init__.py
+++ reverse_search/elasticsearch/__init__.py
@@ -2,4 +2,7 @@ from .search import Elasticsearch
 from .. import config
 
 
-client = Elasticsearch(hosts=config.ELASTICSEARCH["hosts"])
+client = Elasticsearch(
+    hosts=config.ELASTICSEARCH["hosts"],
+    timeout=30, max_retries=10, retry_on_timeout=True
+)
